6e2bca83c442ad054ee3de23209d103ebeeade30
Sean Bethard <sean@seanbethard.info>
2020-05-18 17:12:44 -0500

    sample entities

--- lxpdf/fileops.py
+++ lxpdf/fileops.py
@@ -1,13 +1,21 @@
 import pdftotext
-
+import spacy
+import random
 
 def main(pdf):
+
+  nlp = spacy.load("de_core_news_md")
+  
   with open(pdf, "rb") as f:
     pdf = pdftotext.PDF(f)
-  
-  text = "\n\n".join(pdf)
 
-  print(text)
+  # req: 1GB of temporary memory per 100,000 char
+  text = "\n\n".join(pdf)[1000000:]
+  
+  doc = nlp(text)
+  ents = {(e.text, e.label_) for e in doc.ents}
+
+  print(random.sample(ents, 4))
 
 
 if __name__ == '__main__':
